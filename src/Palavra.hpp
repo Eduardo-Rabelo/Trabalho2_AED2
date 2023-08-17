97810cc65c22dd031faceedf4a8509e58530f419
Eduardo Rabelo Guimarães <dudurg14@gmail.com>
2023-08-16 22:21:40 -0300

    Quarto commit

---
+++ src/Palavra.hpp
@@ -0,0 +1,17 @@
+#ifdef PALAVRA_HPP
+#define PALAVRA_HPP
+#include <iostream>
+using namespace std;
+
+class Palavra
+{
+private:
+    int frequencia;
+    string palavra;
+public:
+    Palavra(/* args */);
+};
+
+Palavra::Palavra(string palavra);
+
+#endif
